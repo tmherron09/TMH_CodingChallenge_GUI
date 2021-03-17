a5192102f3c69a07c462898a2aa64cd5d1e4ba18
Timothy M Herron <tmherron09@gmail.com>
2021-03-17 09:08:24 -0500

    Added DiagDiff Window/Form

--- TMHCodingChallengeHostGUI/DiagonalDifferenceWindow.Designer.cs
+++ TMHCodingChallengeHostGUI/DiagonalDifferenceWindow.Designer.cs
@@ -3,15 +3,11 @@ namespace TMHCodingChallengeHostGUI
 {
     partial class DiagonalDifferenceWindow
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
+
+
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -23,18 +19,47 @@ namespace TMHCodingChallengeHostGUI
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
+
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbArray = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbArray
+            // 
+            this.tbArray.Enabled = false;
+            this.tbArray.Font = new System.Drawing.Font("Fira Code Retina", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tbArray.Location = new System.Drawing.Point(33, 102);
+            this.tbArray.Multiline = true;
+            this.tbArray.Name = "tbArray";
+            this.tbArray.ReadOnly = true;
+            this.tbArray.Size = new System.Drawing.Size(389, 201);
+            this.tbArray.TabIndex = 0;
+            this.tbArray.Text = "1 2 3\r\n4 5 6\r\n7 8 9";
+            this.tbArray.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // DiagonalDifferenceWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "DiagonalDifferenceWindow";
+            this.ClientSize = new System.Drawing.Size(784, 411);
+            this.ControlBox = false;
+            this.Controls.Add(this.tbArray);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DiagonalDifferenceWindow";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Diagonal Difference Challenge";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbArray;
     }
 }
--- TMHCodingChallengeHostGUI/DiagonalDifferenceWindow.cs
+++ TMHCodingChallengeHostGUI/DiagonalDifferenceWindow.cs
@@ -12,9 +12,46 @@ namespace TMHCodingChallengeHostGUI
 {
     public partial class DiagonalDifferenceWindow : Form
     {
+
+        private DiagonalDifference diagDiff = new DiagonalDifference();
+
+        private int Result
+        {
+            get
+            {
+                if(diagDiff == null)
+                {
+                    diagDiff = new DiagonalDifference();
+                    
+                }
+                return diagDiff.DiagonalDifferenceResult();
+            }
+        }
+
         public DiagonalDifferenceWindow()
         {
             InitializeComponent();
+
+            tbArray.ReadOnly = true;
+
+            string[] strArr = new string[diagDiff.InputLength];
+            for(int i = 0; i < diagDiff.InputLength; i++)
+            {
+                string lineStr = "";
+                for(int j = 0; j < diagDiff.InputLength; j++)
+                {
+                    lineStr += diagDiff.InputLength - 1 != j ? diagDiff.Arr[i][j].ToString() + " " : diagDiff.Arr[i][j].ToString();
+                }
+                strArr[i] = lineStr;
+            }
+            tbArray.Lines = strArr;
+
         }
+
+
+
+
+
+
     }
 }
